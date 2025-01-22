018ccedd052d6743e4989df6d0d1dd33254e03c7
Khadaassi <aassikhadija@yahoo.fr>
2025-01-22 15:39:38 +0100

    add profile page and all changes for that

--- MyApp/app/urls.py
+++ MyApp/app/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from .views import HomeView, RegisterView, LoginView, ProfileView, PredictionView
+from .views import HomeView, RegisterView, LoginView, ProfileView, PredictionView, UserInfosView
 
 app_name = 'app'
 urlpatterns = [
     path('home', HomeView.as_view(), name='home'),
     path('register', RegisterView.as_view(), name='register'),
     path('login', LoginView.as_view(), name='login'),
-    path('profile', ProfileView.as_view(), name='profile'),
-    path('predict', PredictionView.as_view(), name='predict'),
+    path('profil', ProfileView.as_view(), name='profil'),
+    path('user_infos', UserInfosView.as_view(), name='user_infos'),
+    path('prediction', PredictionView.as_view(), name='prediction'),
 ]
 
     # path('logout', views.logout, name='logout'),
--- MyApp/app/views.py
+++ MyApp/app/views.py
@@ -4,7 +4,6 @@ from django.views.generic import View
 # Create your views here.
 class HomeView(View):
     template_name = 'app/home.html'
-    # context = {'message': 'Welcome to the Home Page!'}
     def get(self, request):
         context = {'message': 'Welcome to the Home Page!'}
         return render(request, self.template_name, context)
@@ -19,11 +18,16 @@ class RegisterView(View):
         return render(request, self.template_name)
 
 class ProfileView(View):
-    template_name = 'app/profile.html'
+    template_name = 'app/profil.html'
     def get(self, request):
         return render(request, self.template_name)
 
 class PredictionView(View):
     template_name = 'app/prediction.html'
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class UserInfosView(View):
+    template_name = 'app/user_infos.html'
     def get(self, request):
         return render(request, self.template_name)
