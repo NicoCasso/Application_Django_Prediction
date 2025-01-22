6360812c562110edbcde10f47103ede225c68590
John Doe <samikabdani.pro@gmail.com>
2025-01-22 16:41:35 +0100

    mercredi 17h

--- MyApp/app/urls.py
+++ MyApp/app/urls.py
@@ -1,13 +1,28 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 from .views import HomeView, RegisterView, LoginView, ProfileView, PredictionView
+# from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    path('home', HomeView.as_view, name='home'),
-    path('register', RegisterView.as_view, name='register'),
-    path('login', LoginView.as_view, name='login'),
-    path('profile', ProfileView.as_view, name='profile'),
-    path('predict', ProfileView.as_view, name='predict'),
+    path('home', HomeView.as_view(), name='home'),
+    path('register/', RegisterView.inscription, name='register'),
+    # path('login/', LoginView.as_view(),name='login'),
+    path('login/', LoginView.connexion,name='login'),
+    path('profile', ProfileView.as_view(), name='profile'),
+    path('predict', ProfileView.as_view(), name='predict'),
+    # path('', RedirectView.as_view(url='home/'))
     # path('logout', views.logout, name='logout'),
     
 ]
+
+
+
+
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('feature_page_1/', include('feature_page_1.urls')), # Inclure les URLs de l'application blog
+#     path('', RedirectView.as_view(url='feature_page_1/'))
+# ]
--- MyApp/app/views.py
+++ MyApp/app/views.py
@@ -1,18 +1,51 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm 
+from .form import CustomUserCreationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+
+class RegisterView(TemplateView):
+    template_name = 'app/register.html'
+
+    # Vue basée sur une fonction pour le formulaire d'inscription
+    def inscription(request):
+        if request.method == 'POST':
+            form = CustomUserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('connexion')  # Redirection vers la page de connexion après inscription
+        else:
+            form = CustomUserCreationForm()
+
+        return render(request, 'app/register.html', {'form': form})
+
+class LoginView(TemplateView):
+    template_name = 'app/login.html'
+
+    def connexion(request):
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('acceuil')
+            else:
+                messages.error(request, 'Nom d\'utilisateur ou mot de passe incorrect.')
+        return render(request, 'app/login.html')
 
 # Create your views here.
 class HomeView(TemplateView):
-    template_name = 'home.html'
+    template_name = 'app/home.html'
 
-class LoginView(TemplateView):
-    template_name = 'login.html'
-
-class RegisterView(TemplateView):
-    template_name = 'register.html'
 
 class ProfileView(TemplateView):
-    template_name = 'profile.html'
+    template_name = 'app/profile.html'
 
 class PredictionView(TemplateView):
-    template_name = 'prediction.html'
+    template_name = 'app/prediction.html'
